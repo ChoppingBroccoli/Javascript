b0d45d24a7f343e7063bcd349dc0c5b2289586e1
Justin <ChoppingBroccoli@users.noreply.github.com>
2018-08-02 23:13:35 -0400

    Initial commit

---
+++ printNumbers.js
@@ -0,0 +1,10 @@
+// 1) Print all numbers between -10 and 19
+var num1 = 19;
+while(num1 <= 19 && num1 >= -10) {
+	console.log(num1)
+	num1--
+}
+
+// 2) Print all EVEN numbers between 10 and 40
+// 3) Print all ODD numbers between 300 and 333
+// 4) Print all numbers divisible by 5 AND 3 between 5 and 50
